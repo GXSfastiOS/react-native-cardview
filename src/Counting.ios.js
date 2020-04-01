454178ba86c049685dc1a2504f298652f9c873e3
guo_shanghaiqiuxiang <18223804622@qq.com>
2020-04-01 16:48:22 +0800

    修改

--- src/Counting.ios.js
+++ src/Counting.ios.js
@@ -1,35 +1,36 @@
 
-import React, { Component, PropTypes } from 'react';
-import { requireNativeComponent,NativeModules } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { requireNativeComponent, NativeModules } from 'react-native';
 
 var Count = requireNativeComponent('Counting', Counting);
 var CountManager = NativeModules.CountingManager;
 
 export default class Counting extends Component {
   static defaultProps = {
-    text:'0',
-    fontProperty:{
-      fontSize:17,
-      textAlignment:'center',
-      boldSystem:false,
-      method:'Linear'
+    text: '0',
+    fontProperty: {
+      fontSize: 17,
+      textAlignment: 'center',
+      boldSystem: false,
+      method: 'Linear'
     },
-    textColor:'black'
+    textColor: 'black'
   }
 
   static propTypes = {
-    text:PropTypes.string,
-    fontProperty:PropTypes.element,        
-    textColor:PropTypes.string, 
-    onFinish:PropTypes.func
+    text: PropTypes.string,
+    fontProperty: PropTypes.element,
+    textColor: PropTypes.string,
+    onFinish: PropTypes.func
   }
-  
-  countingStart(start,end,duration){
-    CountManager.startValue(start,end,duration);
+
+  countingStart(start, end, duration) {
+    CountManager.startValue(start, end, duration);
   }
-  
-render() {
-    return <Count {...this.props}/>;
+
+  render() {
+    return <Count {...this.props} />;
   }
 }
 
